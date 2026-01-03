c82d58436c93b3385b38022fd77f6759e9018788
rachel <seeyou0727@gmail.com>
2026-01-03 22:39:22 +0800

    complete Part 4-2

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ async def user_signup(body: dict = Body(...)):
 		cursor.execute("SELECT id FROM users WHERE email=%s", (email,))
 		existing = cursor.fetchone()
 		if existing:
-			return JSONResponse(status_code=400, content={"error": True, "message": "註冊失敗，重複的 Email"})
+			return JSONResponse(status_code=400, content={"error": True, "message": "Email已經註冊帳戶"})
 		
 		# 把明文密碼做 bcrypt 雜湊，再存進資料庫
 		pw_bytes = password.encode("utf-8") # 人類看的 str → 電腦看的 bytes
@@ -136,13 +136,13 @@ async def signin(body: dict = Body(...)):
 		row = cursor.fetchone()
 		
 		if not row: # 找不到使用者
-			return JSONResponse(status_code=400, content={"error": True, "message": "登入失敗，帳號或密碼錯誤"}) # 不要讓使用者知道是帳號還是密碼錯誤
+			return JSONResponse(status_code=400, content={"error": True, "message": "電子郵件或密碼錯誤"}) # 不要讓使用者知道是帳號還是密碼錯誤
 		pw_ok = bcrypt.checkpw(
 			password.encode("utf-8"),
 			row["password_hash"].encode("utf-8"),
 		) # checkpw()從第二個參數（hash）裡拆出演算法版本、cost、salt 後，用同一組設定 + 同一個 salt，拿第一個參數的密碼重算一次 bcrypt，比較「重算出來的 hash」和「資料庫這串 hash」是不是一樣，再回傳 True（密碼正確） / False
 		if not pw_ok: # 明碼不符合雜湊
-			return JSONResponse(status_code=400, content={"error": True, "message": "登入失敗，帳號或密碼錯誤"})
+			return JSONResponse(status_code=400, content={"error": True, "message": "電子郵件或密碼錯誤"})
 		
 		token = make_token(row["id"], row["name"], row["email"]) # 產生 JWT token
 
